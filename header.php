8a74326c2f7c6a9df447c7c0794786a98358e047
JadeU <jadeunderwood3@gmail.com>
2020-03-05 10:19:21 -0800

    Add logo svg to images folder, add logo to site header

--- header.php
+++ header.php
@@ -20,11 +20,17 @@
 		<div id="page" class="hfeed site">
 			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
 
-			<header id="masthead" class="site-header" role="banner">
+			<header id="masthead" class="site-header" role="banner" >
 				<div class="site-branding">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
+
+				<div class="header-logo">
+						<a href="http://localhost:8888/quotes-on-dev/">
+							<img src="<?php echo get_template_directory_uri();?>/images/qod-logo.svg"   alt="Quotes on Dev logo">
+						</a>
+					</div>	
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
